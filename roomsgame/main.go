2b2759638e63841d7a07cc54230bc66b29ac5d6a
yaspe <vasily.matveev@gmail.com>
2016-10-13 12:11:58 +0300

    style fixes

--- roomsgame/main.go
+++ roomsgame/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
 	"roomsgame/core"
 	"time"
@@ -12,20 +11,18 @@ func main() {
 
 	var house = core.NewHouse()
 
-	fmt.Println("")
-
 	var rooms = house.GetRooms()
 	for y := range rooms {
 		for x := range rooms[y] {
 			room_type := rooms[y][x].GetType()
-			if room_type == 0 {
-				fmt.Print(" ")
-			} else if room_type == 1 {
-				fmt.Print("^")
+			if room_type == core.RT_PASSAGE {
+				print(" ")
+			} else if room_type == core.RT_ENTRANCE {
+				print("^")
 			} else {
-				fmt.Print("X")
+				print("X")
 			}
 		}
-		fmt.Println("")
+		println()
 	}
 }
